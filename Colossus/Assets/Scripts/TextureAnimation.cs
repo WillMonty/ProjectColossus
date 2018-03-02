79a274aac1ac83495741f84171af6fa7f8ae3cfe
Jared White <jmw8974@rit.edu>
2018-03-02 16:11:25 -0500

    Added a conveyor belt testing scene; animation script created but not functional yet

---
+++ Colossus/Assets/Scripts/TextureAnimation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Material))]
+
+public class TextureAnimation : MonoBehaviour {
+
+    public Material mat;
+
+	// Use this for initialization
+	void Start () {
+        mat = GetComponent<Material>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(mat.mainTextureOffset);
+        mat.mainTextureOffset += new Vector2(1f, 1f);
+	}
+}
